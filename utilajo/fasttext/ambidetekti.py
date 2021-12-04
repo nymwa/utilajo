2aaddfbee1bd1f8474c2715aac9e53f9339a229a
nymwa <nymwa0@gmail.com>
2021-12-04 17:30:06 +0900

    add

--- utilajo/fasttext/ambidetekti.py
+++ utilajo/fasttext/ambidetekti.py
@@ -2,19 +2,39 @@ import sys
 from argparse import ArgumentParser
 from fasttext import load_model
 
-def detect(model_path, src_lang, trg_lang, k=1, rev=False):
+def make_printer(src_label, trg_label, k, p, rev):
+
+    def k_output(src, trg, lang_labels, probs):
+        if ((src_label in src_lang) and (trg_label in trg_lang)) ^ rev:
+            print('{}\t{}'.format(src, trg))
+
+    def p_output(src, trg, lang_labels, probs):
+        if ((((src_label == src_lang[0])
+            and (trg_label == trg_lang[0]))
+            and (probs[0] >= p))
+            ^ rev):
+            print('{}\t{}'.format(src, trg))
+
+    assert (k == 1) or (p is None)
+    if (k == 1) and (p is not None):
+        f = p_output
+    else:
+        f = k_output
+    return f
+
+
+def detect(model_path, src_lang, trg_lang, k=1, p=None, rev=False):
     src_label = '__label__{}'.format(src_lang)
     trg_label = '__label__{}'.format(trg_lang)
     model = load_model(model_path)
+    printer = make_printer(src_label, trg_label, k, p, rev)
 
     for line in sys.stdin:
         line = line.rstrip('\n')
         src, trg = line.split('\t')
         src_lang = model.predict(src, k=k)[0]
         trg_lang = model.predict(trg, k=k)[0]
-        if ((src_label in src_lang) and (trg_label in trg_lang)) ^ rev:
-            line = '{}\t{}'.format(src, trg)
-            print(line)
+        printer(src, trg, lang_labels, probs)
 
 
 def parse_args():
@@ -23,11 +43,12 @@ def parse_args():
     parser.add_argument('src_lang')
     parser.add_argument('trg_lang')
     parser.add_argument('-k', type=int, default=1)
+    parser.add_argument('-p', type = float, default = None)
     parser.add_argument('-r', '--reverse', dest='rev', action = 'store_true')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    detect(args.model, args.src_lang, args.trg_lang, args.k, args.rev)
+    detect(args.model, args.src_lang, args.trg_lang, args.k, args.p, args.rev)
 
--- utilajo/fasttext/detekti.py
+++ utilajo/fasttext/detekti.py
@@ -2,27 +2,46 @@ import sys
 from argparse import ArgumentParser
 from fasttext import load_model
 
-def detect(model_path, k=1, lang='en', rev=False):
+def make_printer(target_label, k, p, rev):
+
+    def k_output(x, lang_labels, probs):
+        if (target_label in lang_labels) ^ rev:
+            print(x)
+
+    def p_output(x, lang_labels, probs):
+        if ((target_label == lang_labels[0]) and (probs[0] >= p)) ^ rev:
+            print(x)
+
+    assert (k == 1) or (p is None)
+    if (k == 1) and (p is not None):
+        f = p_output
+    else:
+        f = k_output
+    return f
+
+
+def detect(model_path, k=1, p=None, lang='en', rev=False):
     target_label = '__label__{}'.format(lang)
     model = load_model(model_path)
+    printer = make_printer(target_label, k, p, rev)
 
     for line in sys.stdin:
         line = line.strip()
-        lang_labels = model.predict(line, k=k)[0]
-        if (target_label in lang_labels) ^ rev:
-            print(line)
+        lang_labels, probs = model.predict(line, k=k)
+        printer(x, lang_labels, probs)
 
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('model')
-    parser.add_argument('-l', '--lang', default='en')
-    parser.add_argument('-k', type=int, default=1)
-    parser.add_argument('-r', '--reverse', dest='rev', action = 'store_true')
+    parser.add_argument('-k', type = int, default = 1)
+    parser.add_argument('-p', type = float, default = None)
+    parser.add_argument('-l', '--lang', default = 'en')
+    parser.add_argument('-r', '--reverse', dest = 'rev', action = 'store_true')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    detect(args.model, args.k, args.lang, args.rev)
+    detect(args.model, args.k, args.p, args.lang, args.rev)
 
